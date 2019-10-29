4286ccb8e22b0b4cf3434a7274f3231abecff884
Your Name <you@example.com>
2019-10-29 10:40:15 +0200

    html užduotis

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-pint 'lol';
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>As valdau html</title>
+  </head>
+  <body>
+    <h1>Danas  HTML asas</h1>
+    <p>Moku kuti statini turini</p>
+  </body>
+</html>
